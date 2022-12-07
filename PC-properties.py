e7499c71c72f9634c8f575d53904a07e75af305e
Omoyemi Quadri <56293002+omoyemiquadri@users.noreply.github.com>
2022-12-07 15:34:38 -0600

    Add files via upload

---
+++ PC-properties.py
@@ -0,0 +1,16 @@
+# A python program to Find all your Laptop properties.
+import os,platform
+import time
+
+print('Operating system name: ', os.name)
+print('Platform name        : ',platform.system())
+print('This Laptop Processor: ',platform.processor())
+print('Laptop Machine type  : ',platform.machine())
+print('This Laptop Version  : ',platform.version())
+print('This Laptop Username : ',platform.node())
+print('')
+print('')
+print('')
+print('Compile by: Horla_Barnabas')
+
+time.sleep(20)
